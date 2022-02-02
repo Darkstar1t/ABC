1825cc3737445abed88f8a01fc42b6d2ac896e81
Antonio Pastore <66266649+Darkstar1t@users.noreply.github.com>
2022-02-02 06:01:51 -0800

    Initialize feature-java

---
+++ packages/cli/bin/scripts.js
@@ -0,0 +1,434 @@
+#!/usr/bin/env node
+
+"use strict";
+
+process.on("uncaughtException", (err) => {
+  // Format any uncaught exceptions
+  console.error("\n" + (err ? err.stack || err : "Uncaught exception") + "\n");
+  process.exit(1);
+});
+process.on("unhandledRejection", (err) => {
+  throw err;
+});
+
+require("source-map-support").install();
+
+const path = require("path");
+const fs = require("fs-extra");
+const yargs = require("yargs");
+const chalk = require("chalk");
+const spawn = require("cross-spawn");
+const {
+  logger,
+  initializeLogger,
+  Packager,
+  Update,
+  getCdkVersion,
+} = require("@serverless-stack/core");
+
+const packageJson = require("../package.json");
+const paths = require("../scripts/util/paths");
+const cdkOptions = require("../scripts/util/cdkOptions");
+const {
+  prepareCdk,
+  loadCache,
+  updateCache,
+} = require("../scripts/util/cdkHelpers");
+
+const sstVersion = packageJson.version;
+const cdkVersion = getCdkVersion();
+
+const args = process.argv.slice(2);
+
+const script = args[0];
+const scriptArgs = args.slice(1);
+
+const cmd = {
+  s: "sst",
+  cdk: "cdk",
+  diff: "diff",
+  test: "test",
+  start: "start",
+  build: "build",
+  deploy: "deploy",
+  remove: "remove",
+  addCdk: "add-cdk",
+  update: "update",
+};
+
+const internals = {
+  [cmd.diff]: require("../scripts/diff"),
+  [cmd.start]: require("../scripts/start"),
+  [cmd.build]: require("../scripts/build"),
+  [cmd.deploy]: require("../scripts/deploy"),
+  [cmd.remove]: require("../scripts/remove"),
+  [cmd.addCdk]: require("../scripts/add-cdk"),
+};
+
+const DEFAULT_STAGE = "dev";
+const DEFAULT_NAME = "my-app";
+const DEFAULT_REGION = "us-east-1";
+const DEFAULT_LINT = true;
+const DEFAULT_TYPE_CHECK = true;
+const DEFAULT_ESBUILD_CONFIG = undefined;
+
+function getCliInfo() {
+  const usingYarn = Packager.getManager(paths.appPath).type === "yarn";
+
+  return {
+    cdkVersion,
+    yarn: usingYarn,
+    npm: !usingYarn,
+    // Options that'll be passed into CDK
+    cdkOptions: {
+      ...cdkOptions,
+      roleArn: argv.roleArn,
+      verbose: argv.verbose ? 2 : 0,
+      noColor: process.env.NO_COLOR === "true",
+    },
+  };
+}
+
+function addOptions(currentCmd) {
+  return function (yargs) {
+    yargs
+      .option("stage", {
+        type: "string",
+        describe: "The stage you want to deploy to",
+      })
+      .option("region", {
+        type: "string",
+        describe: "The region you want to deploy to",
+      })
+      .option("role-arn", {
+        type: "string",
+        describe: "ARN of Role to use when invoking CloudFormation",
+      });
+
+    if (currentCmd === cmd.deploy || currentCmd === cmd.remove) {
+      yargs.positional("stack", {
+        type: "string",
+        describe: "Specify a stack, if you have multiple stacks",
+      });
+    }
+
+    if (currentCmd === cmd.deploy || currentCmd === cmd.start) {
+      yargs.option("outputs-file", {
+        type: "string",
+        describe: "Path to file where the stack outputs will be written",
+      });
+    }
+
+    if (currentCmd === cmd.start) {
+      yargs
+        .option("increase-timeout", {
+          type: "boolean",
+          describe:
+            "Increase the Function timeouts to allow testing with breakpoints",
+          default: false,
+        })
+        .option("port", {
+          type: "number",
+          describe:
+            "Configure the port for local Lambda Runtime API server. Default is 12577.",
+          default: 12577,
+        });
+    }
+  };
+}
+
+function applyConfig(argv) {
+  const configPath = path.join(paths.appPath, "sst.json");
+
+  if (!fs.existsSync(configPath)) {
+    exitWithMessage(
+      `\nAdd the ${chalk.bold(
+        "sst.json"
+      )} config file in your project root to get started. Or use the ${chalk.bold(
+        "create-serverless-stack"
+      )} CLI to create a new project.\n`
+    );
+  }
+
+  let config;
+
+  try {
+    config = fs.readJsonSync(configPath);
+  } catch (e) {
+    exitWithMessage(
+      `\nThere was a problem reading the ${chalk.bold(
+        "sst.json"
+      )} config file. Make sure it is in valid JSON format.\n`
+    );
+  }
+
+  if (!config.name || config.name.trim() === "") {
+    exitWithMessage(
+      `\nGive your Serverless Stack app a ${chalk.bold(
+        "name"
+      )} in the ${chalk.bold("sst.json")}.\n\n  "name": "my-sst-app"\n`
+    );
+  }
+
+  config.name = config.name || DEFAULT_NAME;
+  config.stage = argv.stage || config.stage || DEFAULT_STAGE;
+  config.lint = config.lint === false ? false : DEFAULT_LINT;
+  config.region = argv.region || config.region || DEFAULT_REGION;
+  config.typeCheck = config.typeCheck === false ? false : DEFAULT_TYPE_CHECK;
+  config.main = config.main || getDefaultMainPath();
+  config.esbuildConfig = config.esbuildConfig || DEFAULT_ESBUILD_CONFIG;
+
+  return config;
+}
+
+function getDefaultMainPath() {
+  let mainPath = path.join("lib", "index.ts");
+  if (!fs.existsSync(path.join(paths.appPath, mainPath))) {
+    mainPath = path.join("lib", "index.js");
+  }
+  return mainPath;
+}
+
+function cleanupBuildDir(script) {
+  // Backup cache data in the .build directory and recreate it
+  if (script === cmd.start) {
+    const cacheData = loadCache();
+    fs.emptyDirSync(paths.appBuildPath);
+    updateCache(cacheData);
+  } else {
+    fs.emptyDirSync(paths.appBuildPath);
+  }
+}
+
+/**
+ * If `npm run` is used to execute these commands, you need to add `--` before
+ * the options. If it's not used, the command will run but the options will not be
+ * set correctly. The region or the stage might get set as the stack. This
+ * function simply checks if the stack is set to a common stage name or a region.
+ * And shows a warning.
+ */
+function checkNpmScriptArgs() {
+  const commonStageAndRegions = [
+    "qa",
+    "dev",
+    "prod",
+    "stage",
+    "staging",
+    "preprod",
+    "production",
+    "development",
+    "eu-west-1",
+    "eu-west-2",
+    "sa-east-1",
+    "us-east-1",
+    "us-east-2",
+    "us-west-1",
+    "us-west-2",
+    "ap-south-1",
+    "ca-central-1",
+    "eu-central-1",
+    "ap-northeast-2",
+    "ap-southeast-1",
+    "ap-southeast-2",
+    "ap-northeast-1",
+  ];
+
+  if (commonStageAndRegions.indexOf(argv.stack) !== -1) {
+    logger.warn(
+      chalk.yellow(
+        `\nWarning: It looks like you might be setting the stack option to "${argv.stack}" by mistake. If you are using "npm run", make sure to add "--" before the options. For example, "npm run deploy -- --stage prod".\n`
+      )
+    );
+  }
+}
+
+function exitWithMessage(message) {
+  // Move newline before message
+  if (message.indexOf("\n") === 0) {
+    logger.info("");
+  }
+  logger.error(message.trimStart());
+
+  process.exit(1);
+}
+
+const argv = yargs
+  .parserConfiguration({ "boolean-negation": false })
+
+  .usage(`${cmd.s} <command>`)
+  .demandCommand(1)
+
+  .option("no-color", {
+    default: false,
+    type: "boolean",
+    desc: "Remove colors and other style from console output",
+  })
+  .option("verbose", {
+    default: false,
+    type: "boolean",
+    desc: "Show more debug info in the output",
+  })
+
+  .command(cmd.start, "Work on your SST app locally", addOptions(cmd.start))
+  .command(
+    `${cmd.diff} [stacks..]`,
+    "Compares all the stacks in your app with the deployed stacks"
+  )
+  .command(
+    `${cmd.deploy} [stack]`,
+    "Deploy all your stacks to AWS",
+    addOptions(cmd.deploy)
+  )
+  .command(
+    `${cmd.remove} [stack]`,
+    "Remove all your stacks and all of their resources from AWS",
+    addOptions(cmd.remove)
+  )
+  .command(
+    cmd.build,
+    "Build your app and synthesize your stacks",
+    addOptions(cmd.build)
+  )
+  .command(cmd.test, "Run your tests")
+  .command(cmd.cdk, "Access the AWS CDK CLI")
+  .command(cmd.update, "Update SST and CDK packages to the latest versions")
+  .command(
+    `${cmd.addCdk} [packages..]`,
+    "Installs the given CDK package(s) in your app",
+    {
+      dev: {
+        default: false,
+        type: "boolean",
+        desc: "Install as a dev dependency",
+      },
+      "dry-run": {
+        default: false,
+        type: "boolean",
+        desc: "Do not install, but show the install command",
+      },
+    }
+  )
+
+  .example([
+    [`$0 ${cmd.start}`, "Start using the defaults"],
+    [`$0 ${cmd.remove} my-s3-stack`, "Remove a specific stack"],
+    [
+      `$0 ${cmd.deploy} --stage prod --region us-west-1`,
+      "Deploy to a stage and region",
+    ],
+  ])
+
+  .version(
+    true,
+    "Show the version of SST and CDK",
+    `SST: ${sstVersion}\nCDK: ${cdkVersion}`
+  )
+  .alias("version", "v")
+  .help("help")
+  .alias("help", "h")
+  .epilogue("For more information, visit www.serverless-stack.com")
+
+  .wrap(yargs.terminalWidth())
+
+  .fail((msg, err) => {
+    if (err) throw err;
+
+    console.log(chalk.red(msg) + "\n");
+
+    yargs.showHelp();
+
+    process.exit(1);
+  })
+  .parse();
+
+// Disable color
+if (!process.stdout.isTTY || argv.noColor) {
+  process.env.NO_COLOR = "true";
+  chalk.level = 0;
+}
+
+// Set debug flag
+if (argv.verbose) {
+  process.env.DEBUG = "true";
+}
+
+// Cleanup build dir
+cleanupBuildDir(script);
+
+// Initialize logger after .build diretory is created, in which the debug log will be written
+initializeLogger(paths.appBuildPath);
+logger.debug("SST:", sstVersion);
+logger.debug("CDK:", cdkVersion);
+
+// Parse cli input and load config
+const cliInfo = getCliInfo();
+const config = applyConfig(argv);
+
+switch (script) {
+  case cmd.diff:
+  case cmd.build:
+  case cmd.deploy:
+  case cmd.remove: {
+    if (cliInfo.npm) {
+      checkNpmScriptArgs();
+    }
+
+    // Prepare app
+    prepareCdk(argv, cliInfo, config)
+      .then(() => internals[script](argv, config, cliInfo))
+      .catch((e) => exitWithMessage(e.message));
+
+    break;
+  }
+  case cmd.start:
+  case cmd.addCdk: {
+    internals[script](argv, config, cliInfo).catch((e) => {
+      logger.debug(e);
+      exitWithMessage(e.message);
+    });
+
+    break;
+  }
+  case cmd.cdk:
+  case cmd.test: {
+    // Prepare app
+    prepareCdk(argv, cliInfo, config)
+      .then(() => {
+        const result = spawn.sync(
+          "node",
+          [require.resolve("../scripts/" + script)].concat(scriptArgs),
+          { stdio: "inherit" }
+        );
+        if (result.signal) {
+          if (result.signal === "SIGKILL") {
+            exitWithMessage(
+              "The command failed because the process exited too early. " +
+                "This probably means the system ran out of memory or someone called " +
+                "`kill -9` on the process."
+            );
+          } else if (result.signal === "SIGTERM") {
+            exitWithMessage(
+              "The command failed because the process exited too early. " +
+                "Someone might have called `kill` or `killall`, or the system could " +
+                "be shutting down."
+            );
+          }
+          exitWithMessage(
+            "The command failed because the process exited too early."
+          );
+        }
+        process.exit(result.status);
+      })
+      .catch((e) => exitWithMessage(e.message));
+    break;
+  }
+  case cmd.update:
+    Update.run({
+      rootDir: process.cwd(),
+      verbose: argv.verbose,
+    });
+    break;
+  default:
+    console.log('Unknown script "' + script + '".');
+    break;
+}
---
+++ www/docs/constructs/App.md
@@ -0,0 +1,184 @@
+---
+description: "Docs for the sst.App construct in the @serverless-stack/resources package"
+---
+
+The `App` construct extends [`cdk.App`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_core.App.html) and is used internally by SST to:
+
+- Automatically prefix stack names with the stage and app name
+- Deploy the entire app using the same AWS profile and region
+
+It is made available as the `app` in the `lib/index.js` of your SST app.
+
+```js
+export default function main(app) {
+  new MySampleStack(app, "sample");
+}
+```
+
+Since it is initialized internally, the props that are passed to `App` cannot be changed.
+
+## Examples
+
+### Accessing app properties
+
+The properties of the app can be accessed in the `lib/index.js` as:
+
+```js
+app.name;
+app.stage;
+app.region;
+app.account;
+```
+
+### Specifying default function props
+
+You can set some function props and have them apply to all the functions in the app.
+
+```js title="lib/index.js"
+export default function main(app) {
+  app.setDefaultFunctionProps({
+    timeout: 20,
+    memorySize: 512,
+    runtime: "go1.x",
+    environment: { TABLE_NAME: "NOTES_TABLE" },
+  });
+
+  // Add stacks
+}
+```
+
+Or if you need to access the `Stack` scope, you can pass in a callback.
+
+```js title="lib/index.js"
+import { StringParameter } from "@aws-cdk/aws-ssm";
+
+export default function main(app) {
+  app.setDefaultFunctionProps((stack) => ({
+    timeout: 20,
+    memorySize: 512,
+    runtime: "go1.x",
+    environment: {
+      API_KEY: StringParameter.valueFromLookup(stack, "my_api_key"),
+    },
+  }));
+
+  // Add stacks
+}
+```
+
+You can also call `setDefaultFunctionProps` multiple times, and the props from each call will be merged. If a property is set more than once, the last value will be taken. Except for the `environment`, the `layers`, and the `permissions` properties, that will be merged.
+
+```js title="lib/index.js"
+export default function main(app) {
+  app.setDefaultFunctionProps({
+    timeout: 15,
+    memorySize: 256,
+    environment: { TABLE_NAME: "NOTES_TABLE" },
+  });
+
+  app.setDefaultFunctionProps({
+    timeout: 20,
+    environment: { BUCKET_NAME: "UPLAOD_BUCKET" },
+  });
+
+  // Add stacks
+}
+```
+
+So in the above example, the functions by default will have a `timeout` of `20 seconds`, `memorySize` of `256MB`, and both the `TABLE_NAME` and the `BUCKET_NAME` environment variables set.
+
+### Setting a default removal policy
+
+You can set a removal policy to apply to all the resources in the app. This is useful for ephemeral environments that need to clean up all their resources on removal.
+
+``` js title="lib/index.js"
+import { RemovalPolicy } from "@aws-cdk/core";
+
+export default function main(app) {
+  // Remove all resources when the dev stage is removed
+  if (app.stage === "dev") {
+    app.setDefaultRemovalPolicy(RemovalPolicy.DESTROY);
+  }
+
+  // Add stacks
+}
+```
+
+Note that, the [`setDefaultRemovalPolicy`](#setdefaultremovalpolicy) method isn't meant to be used for production environments.
+
+## Properties
+
+The following properties are made available in addition to the properties of [`cdk.App`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_core.App.html#properties).
+
+### name
+
+_Type_ : `string`
+
+The name of the app. This comes from the `name` in your `sst.json`.
+
+### stage
+
+_Type_ : `string`
+
+The stage the app is being deployed to. If this is not specified as the `--stage` option in the CLI, it'll default to the `stage` in your `sst.json`.
+
+### region
+
+_Type_ : `string`
+
+The region the app is being deployed to. If this is not specified as the `--region` option in the CLI, it'll default to the `region` in your `sst.json`.
+
+### account
+
+_Type_ : `string`
+
+The AWS account the app is being deployed to. This comes from the IAM credentials being used to run SST.
+
+## Methods
+
+### setDefaultFunctionProps
+
+```ts
+setDefaultFunctionProps(props: FunctionProps | ((stack: cdk.Stack) => FunctionProps))
+```
+
+_Parameters_
+
+- **props** `FunctionProps | ((stack: cdk.Stack) => FunctionProps)`
+
+The default function props to be applied to all the Lambda functions in the app. These default values will be overridden if a [`Function`](Function.md) sets its own props. Except for the `environment`, the `layers`, and the `permissions` properties, which will be merged.
+
+Takes a [`FunctionProps`](Function.md#functionprops). Or a callback function takes [`cdk.Stack`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_core.Stack.html) that returns a [`FunctionProps`](Function.md#functionprops).
+
+### setDefaultRemovalPolicy
+
+```ts
+setDefaultRemovalPolicy(policy: cdk.RemovalPolicy)
+```
+
+_Parameters_
+
+- **props** [`cdk.RemovalPolicy`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_core.RemovalPolicy.html)
+
+The default removal policy that'll be applied to all the resources in the app. This can be useful to set ephemeral (dev or feature branch) environments to remove all the resources on deletion.
+
+:::danger
+Make sure to not set the default removal policy to `DESTROY` for production environments.
+:::
+
+
+### logicalPrefixedName
+
+```ts
+logicalPrefixedName(logicalName: string): string
+```
+
+_Parameters_
+
+- **logicalName** `string`
+
+_Returns_
+
+- `string`
+
+Use this method to prefix resource names in your stacks to make sure they don't thrash when deployed to different stages in the same AWS account. This method will prefix a given resource name with the stage and app name. Using the format `${stage}-${name}-${logicalName}`.
